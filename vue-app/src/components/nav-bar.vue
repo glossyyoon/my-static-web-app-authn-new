bff8de8885bc5022b60065fafa1245d4a1b09285
glossyyoon <mypju@naver.com>
2021-08-06 10:51:25 +0900

    error

--- vue-app/src/components/nav-bar.vue
+++ vue-app/src/components/nav-bar.vue
@@ -2,9 +2,31 @@
 export default {
   name: 'NavBar',
   data() {
-    return {};
+    return {
+      userInfo: {
+        type: Object,
+        default() {},
+      },
+      providers: ['twitter', 'github', 'aad'],
+      redirect: window.location.pathname,
+    };
   },
-  methods: {},
+  methods: {
+    async getUserInfo() {
+      try {
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+        return clientPrincipal;
+      } catch (error) {
+        console.error('No profile could be found');
+        return undefined;
+      }
+    },
+  },
+  async created() {
+    this.userInfo = await this.getUserInfo();
+  },
 };
 </script>
 <template>
@@ -16,5 +38,35 @@ export default {
         <router-link to="/about">About</router-link>
       </ul>
     </nav>
+    <!-- Login and logout buttons -->
+    <nav class="menu auth">
+      <p class="menu-label">Auth</p>
+      <div class="menu-list auth">
+        <template v-if="!userInfo">
+          <template v-for="provider in providers">
+            <a
+              :key="provider"
+              :href="`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`"
+            >
+              {{ provider }}
+            </a>
+          </template>
+        </template>
+        <a
+          v-if="userInfo"
+          :href="`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`"
+        >
+          Logout
+        </a>
+      </div>
+    </nav>
+    <!-- end of login and logout buttons -->
+    <!-- User infos -->
+    <div class="user" v-if="userInfo">
+      <p>Welcome</p>
+      <p>{{ userInfo.userDetails }}</p>
+      <p>{{ userInfo.identityProvider }}</p>
+    </div>
+    <!-- End of user infos -->
   </div>
 </template>
